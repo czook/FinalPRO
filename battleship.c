d3eab0ce50be69c202ab61bdb751c0adf6f76b19
Corbeno <corbin@kemsfmaily.us>
2019-11-22 13:08:23 -0600

    added start for battleship. Build off this

---
+++ battleship.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//uncomment one of these includes depending on the computer you're using 
+//(add one if you have something different)
+#include <ncurses/ncurses.h> //lab computers
+//#include<curses.h> //corbin's laptop
+
+#define BOARD_LENGTH 10
+#define BOARD_WIDTH 10
+
+
+void mian(){
+    int playerGrid[BOARD_LENGTH][BOARD_WIDTH];
+    int computerGrid[BOARD_LENGTH][BOARD_WIDTH];
+
+}
