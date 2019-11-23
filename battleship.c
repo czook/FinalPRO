3e885d842a38ce309bb9006aaac727964b5fc7b7
Camdyn Zook <dodabaggypants@gmail.com>
2019-11-22 23:37:35 -0600

    Added basic outline
    + Structs
    + CHAR's for the boats
    + More libraries
    + prototypes of possible functions

--- battleship.c
+++ battleship.c
@@ -1,16 +1,41 @@
 #include<stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
 //uncomment one of these includes depending on the computer you're using 
 //(add one if you have something different)
-#include <ncurses/ncurses.h> //lab computers
-//#include<curses.h> //corbin's laptop
+//#include <ncurses/ncurses.h> //lab computers
+#include<curses.h> //corbin's laptop
 
 #define BOARD_LENGTH 10
 #define BOARD_WIDTH 10
+//structs are used to check if parts of the boats are hit.
+//using int {varible} = 1 or 0 depending if it is true or false
+struct carrier{
+    int point1;
+    int point2;
+    int point3;
+}carrier;
 
+struct patrol{
+    int point1;
+    int point2;
+}patrol;
 
-void mian(){
+struct boat{
+    int point1;
+}boat;
+
+struct Ships{
+    carrier c;
+    patrol p;
+    boat b;
+}Ships;
+
+void main(){
     int playerGrid[BOARD_LENGTH][BOARD_WIDTH];
     int computerGrid[BOARD_LENGTH][BOARD_WIDTH];
+    Ships compShips;
+    Ships playerShips;
 
 }
