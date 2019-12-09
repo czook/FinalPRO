5efbd57394f642ce9d3c94b85370e451e0ab46b1
Corbeno <corbin@kemsfmaily.us>
2019-12-08 22:03:22 -0600

    x

--- battleship.c
+++ battleship.c
@@ -446,13 +446,8 @@ void main()
 		mvprintw(14, 0, "Input where to put your ship %d; Example D4: ", i);
 		getstr(ch);
 
-        placeShip(playerGrid,9,8,1);
         //mvprintw(20,0,"%d, %d ",ch[0]-64-1,ch[1]-48-1); //testing
 
-        mvprintw(21,0,"%c",ch[0]);
-        mvprintw(22,0,"%c",ch[1]);
-        mvprintw(23,0,"%c",ch[2]);
-        mvprintw(24,0,"%c",ch[3]);
         if(ch[1] == '1' && ch[2] == '0'){ //when user enters 10 (quick fix)
             placeShip(playerGrid, ch[0]-64-1, 9, i);
         }else{ //otherwise print normally
