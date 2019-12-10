157d229789674e1c61fcfe26d2f72f57acb7b80f
Corbeno <corbin@kemsfmaily.us>
2019-12-09 18:35:13 -0600

    12/9 big finish

--- battleship.c
+++ battleship.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 //#include <Windows.h>
 //uncomment one of these includes depending on the computer you're using
 //(add one if you have something different)
@@ -18,29 +19,6 @@
 #define BLANK '*'
 //structs are used to check if parts of the boats are hit.
 //using int {varible} = 1 or 0 depending if it is true or false
-/*
-struct carrier
-{
-    int points[3][2];
-};
-
-struct patrol
-{
-    int points[2][2];
-};
-
-struct boat
-{
-    int points[1][2];
-};
-
-struct ships
-{
-    struct carrier c;
-    struct patrol p;
-    struct boat b;
-};
-*/
 
 void draw_character(int row, int column, char use)
 {
@@ -75,18 +53,11 @@ void displayCurrentBoard(int gridData[BOARD_LENGTH][BOARD_WIDTH], int x, int y)
  J | * * * * * * * * * *
 
 PLACE SHIP (1): (input)  (ex D4)
-
-
 ship types:
-
-
     */
-
-    char tmp = 'P'; //placeholder representing starting reference location
-    draw_character(y,x,tmp); //can take out later
     
     //print border info
-    char *temp1 = " 1 2 3 4 5 6 7 8 9 10";
+    char *temp1 = " 0 1 2 3 4 5 6 7 8 9";
 	char *temp2 = "--------------------";
     draw_string(y, x + 3,temp1);
     draw_string(y + 1, x + 3,temp2);
@@ -122,243 +93,6 @@ ship types:
 		}       
     }
 }
-/*
-void placeShips(int *gridData, struct carrier c, struct patrol p, struct boat b){
-    //
-
-    /*
-    //Starting ship placement at [0][0]
-	int x = 0, y = 0;
-    
-	
-    //moving the ships on keypress are hopefully FINISHED
-    //Still need to check if the ships are on top of each other.
-    //(The current numbers(0,7,9) are trying to be the limits of the 10x10 board)
-    //place carrier while loop
-    while (1){
-        int ch = getch();
-        if (ch == 'w'){
-            if(y == 0){
-                y = 7;
-                
-                gridData[0][x] = '*';
-                gridData[1][x] = '*';
-                gridData[2][x] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                gridData[y+2][x] = "*";
-                y--;
-        
-            }
-        }
-        else if (ch == 's'){
-            if (y == 7){
-                y = 0;
-                
-                gridData[7][x] = '*';
-                gridData[8][x] = '*';
-                gridData[9][x] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                gridData[y+2][x] = "*";
-                y++;
-                
-            }
-        }
-        else if (ch == 'd'){
-            if (x == 9){
-                x = 0;
-                
-                gridData[y][9] = '*';
-                gridData[y+1][9] = '*';
-                gridData[y+2][9] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                gridData[y+2][x] = "*";;
-                x++;
-                
-            }
-        }
-        else if (ch == 'a'){
-            if (x == 0){
-                x = 9;
-                
-                gridData[y][0] = '*';
-                gridData[y+1][0] = '*';
-                gridData[y+2][0] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                gridData[y+2][x] = "*";
-                x--
-                
-            }
-        }
-        else if (ch == 'k'){
-            
-            break;
-        }
-    c.points[0] = {x,y};
-	c.points[1] = {x,y+1};
-    c.points[2] = {x,y+2};
-    gridData[y][x] = shipChar;
-    gridData[y+1][x] = shipChar;
-    gridData[y+2][x] = shipChar;
-    displayCurrentBoard(gridData);
-    }
-    if(gridData[0][0] == shipChar && gridData[1][0] == shipChar){
-        x = 1;
-        y = 0;
-    }
-    else{
-        x = 0;
-        y = 0;
-    }
-    //patrol while loop
-    while (1){
-        int ch = getch();
-        if (ch == 'w'){
-            if(y == 0){
-                y = 7;
-                
-                gridData[0][x] = '*';
-                gridData[1][x] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                y--;
-                
-            }
-        }
-        else if (ch == 's'){
-            if (y == 7){
-                y = 0;
-                
-                gridData[7][x] = '*';
-                gridData[8][x] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                y++;
-                
-            }
-        }
-        else if (ch == 'd'){
-            if (x == 9){
-                x = 0;
-                
-                gridData[y][9] = '*';
-                gridData[y+1][9] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*';
-                x++;
-                
-            }
-        }
-        else if (ch == 'a'){
-            if (x == 0){
-                x = 9;
-                
-                gridData[y][0] = '*';
-                gridData[y+1][0] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                gridData[y+1][x] = '*'; 
-                x--
-                
-            }
-        }
-        else if (ch == 'k'){
-            
-            break;
-        }
-    p.points[0] = {x,y};
-	p.points[1] = {x,y+1};
-    gridData[y][x] = shipChar;
-    gridData[y+1][x] = shipChar;
-    displayCurrentBoard(gridData);
-    }
-    y = 0;
-    if(gridData[0][0] == shipChar){
-        if(gridData[0][1] == shipChar){
-            x = 2;
-        }
-        else{
-            x = 1;
-        }
-        
-    }
-    else{
-        x = 0;
-    }
-    //boat while loop
-    while (1){
-        int ch = getch();
-        if (ch == 'w'){
-            if(y == 0){
-                y = 7;
-                gridData[0][x] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                y--;
-            }
-        }
-        else if (ch == 's'){
-            if (y == 7){
-                y = 0;
-                gridData[7][x] = '*';
-            }
-            else{
-                gridData[y][x] = '*';;
-                y++;
-            }
-        }
-        else if (ch == 'd'){
-            if (x == 9){
-                x = 0;
-                gridData[y][9] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                x++;
-            }
-        }
-        else if (ch == 'a'){
-            if (x == 0){
-                x = 9;
-                
-                gridData[y][0] = '*';
-            }
-            else{
-                gridData[y][x] = '*';
-                x--
-                
-            }
-        }
-        else if (ch == 'k'){
-            break;
-        }
-    b.points[0] = {x,y};
-    gridData[y][x] = shipChar;
-    displayCurrentBoard(gridData);
-    }
-	
-}
-*/
-
 
 int placeShip(int gridData[BOARD_LENGTH][BOARD_WIDTH],int x, int y, int shipNum){
     //ship number is 1,2, or 3
@@ -369,11 +103,13 @@ int placeShip(int gridData[BOARD_LENGTH][BOARD_WIDTH],int x, int y, int shipNum)
             break;
         case 2: //2x1 - patrol
             if(gridData[x][y] == 1 || gridData[x+1][y] == 1) return 0;
+            if(x >= 9) return 0;
             gridData[x][y] = 1;
             gridData[x+1][y] = 1;
             break;
         case 3: //3x1 - carrier
             if(gridData[x][y] == 1 || gridData[x+1][y] == 1 || gridData[x+2][y] == 1) return 0;
+            if(x >= 8) return 0;
             gridData[x][y] = 1;
             gridData[x+1][y] = 1;
             gridData[x+2][y] = 1;
@@ -381,23 +117,42 @@ int placeShip(int gridData[BOARD_LENGTH][BOARD_WIDTH],int x, int y, int shipNum)
     }
     return 1;
 }
-
+//shoot function duh!
 int shoot(int gridData[BOARD_LENGTH][BOARD_WIDTH],int x, int y){
-    if(gridData[x][y] == 1){
+    if(gridData[x][y] == 1){ //If there is a ship hidden there
         return 1;
-    }else{
+    } else if (gridData[x][y] == 2 || gridData[x][y] == 3){ //If the spot has already been guessed
+		return 2;
+    } else{ //If there is no ship there
+		return 0;
+	}
+}
+
+//tests user input if it fits certain constraints
+//for DEMO, comment out return 0's
+int testInput(char ch[]){
+    //if user enters a letter not within range (65 to 74)
+    if(!(ch[0] > 64 && ch[0] < 75)){
+        return 0;   
+    }
+
+    //if user enters a number not within range (48 to 57)
+    if(!(ch[1] > 47 && ch[1] < 58)){
         return 0;
     }
+        
+    //if ch is longer or less then 2
+    if(strlen(ch) != 2){
+        return 0;
+    }
+    return 1;
 }
 
 void main()
 {
 	//Initialize screen
 	initscr();
-	//noecho();
-	//cbreak();
-	//nodelay(stdscr, TRUE);
-	//scrollok(stdscr, TRUE);
+
     time_t t;
 	srand((unsigned) time(&t));
 
@@ -407,8 +162,6 @@ void main()
 	int playerGuessGrid[BOARD_LENGTH][BOARD_WIDTH];
 	int computerGrid[BOARD_LENGTH][BOARD_WIDTH];
 	int computerGuessGrid[BOARD_LENGTH][BOARD_LENGTH];
-	//struct ships compShips;
-	//struct ships playerShips;
 
 	//location of displays
 	int playerBoardX = 5;
@@ -420,8 +173,6 @@ void main()
 	int cpuGuessX = 95;
 	int cpuGuessY = 1;
 
-    //short flag = 1;
-
 	//set everything to 0 (for sanity's sake) Might not need this
 	for (int i = 0; i < BOARD_LENGTH; i++) {
 		for (int j = 0; j < BOARD_WIDTH; j++) {
@@ -432,17 +183,11 @@ void main()
 		}
 	}
 
-	/* //testing
-	playerGrid[1][10] = 2;
-	playerGrid[2][4] = 1;
-	playerGrid[9][3] = 1;
-	*/
-
 	//initial displays 
 	displayCurrentBoard(playerGrid, playerBoardX, playerBoardY);
 	displayCurrentBoard(playerGuessGrid, playerGuessX, playerGuessY);
-	displayCurrentBoard(computerGrid, cpuBoardX, cpuBoardY);
-	displayCurrentBoard(computerGuessGrid, cpuGuessX, cpuGuessY);
+	//displayCurrentBoard(computerGrid, cpuBoardX, cpuBoardY); //Comment when actually playing
+	//displayCurrentBoard(computerGuessGrid, cpuGuessX, cpuGuessY); //Comment when actually playing
 
     int placeX, placeY;
 
@@ -450,74 +195,119 @@ void main()
 	char* ch;
 	for (int i = 1; i < 4; i++) {
 		mvprintw(14, 0, "Input where to put your ship %d; Example D4: ", i);
-		getstr(ch);
-
-        //if user enters nothing
-        if(!ch[0]){
+		getstr(ch); //Gets input from the player's keyboard
+        
+        //if input is NOT acceptable
+        if(!testInput(ch)){
+            mvprintw(14, 44, "                   "); //remove previous input
             i -= 1;
-            continue;
+            continue; 
         }
-
-        //if user enters a letter not within range (65 to 74)
-        if(!(ch[0] > 64 && ch[0] < 75)){
-            mvprintw(14, 44, "    "); //remove previous input
-            i -= 1;
-            continue;
-        }
-
-        //if user enters a number not within range (49 to 57)
-        if(!(ch[1] > 48 && ch[1] < 58)){
-            mvprintw(14, 44, "    "); //remove previous input
-            i -=1;
-            continue;
-        }
-
-        //if user enters a two digit number that isn't 10
-        if(!ch[2] || ch[2] != '0'){
-            mvprintw(14, 44, "    "); //remove previous input
-            i -=1;
-            continue;
-        }
-
-        //mvprintw(20,0,"%d, %d ",ch[0]-64-1,ch[1]-48-1); //testing
-        if(ch[1] == '1' && ch[2] == '0'){ //when user enters 10 (quick fix)
-            placeX = ch[0]-64-1;
-            placeY = 9;
-        }else{ //otherwise print normally
-            placeX = ch[0]-64-1;
-            placeY = ch[1]-48-1;
-        }
-
-        if(!placeShip(playerGrid, placeX, placeY, i)){//if it failes to place
+        
+        //main placement
+        placeX = ch[0]-64-1;
+        placeY = ch[1]-48;
+        
+        if(!placeShip(playerGrid, placeX, placeY, i)){//if it fails to place
             i = i - 1;  //redo last step
         } 
 	
-        mvprintw(14, 44, "    "); //remove previous input
+        mvprintw(14, 44, "                      "); //remove previous input
         //update board every time
         displayCurrentBoard(playerGrid, playerBoardX, playerBoardY);
 	}
 
-	mvprintw(14, 0, "                                             ");
+	mvprintw(14, 0, "                                             "); //Clear line after placing ships is done
     
     //place bot ships (randomly)
     for(int j = 1; j < 4; j++){
-        placeShip(computerGrid,rand()%10,rand()%10,j);
-        displayCurrentBoard(computerGrid, cpuBoardX, cpuBoardY);
+        //if is overlapping
+        if(!placeShip(computerGrid,rand()%10,rand()%10,j)){
+            j -= 1;
+            continue;
+        }
+        //displayCurrentBoard(computerGrid, cpuBoardX, cpuBoardY); //Comment when actually playing
     }
     
+	int playerScore = 0; //Score 1-6
+	int computerScore = 0; //Score 1-6
+
     //Main loop (game start)
-    while (1){
+    while (playerScore < 6 && computerScore < 6){
         //loop through player and ship picking locations to shoot
         
+		//If guess is valid 
+		int endTurn = 0;
         //player shoot
-        mvprintw(14,0, "Pick a location to shoot: ");
-        getstr(ch);
-        
-        if(shoot(computerGrid,ch[0]-64-1,ch[1]-48-1)){ //if it hits
-            
+		do {
+			mvprintw(14, 0, "Pick a location to shoot: ");
+			getstr(ch);
+
+            //if input is NOT acceptable
+            if(!testInput(ch)){
+                mvprintw(14, 26, "              "); //remove previous input
+                continue; 
+            }
+
+			if(shoot(computerGrid, ch[0] - 64 - 1, ch[1] - 48) == 1) { //if it hits
+				endTurn = 1; 
+				playerGuessGrid[ch[0] - 64 - 1][ch[1] - 48] = 2;
+				computerGrid[ch[0] - 64 - 1][ch[1] - 48] = 2;
+				playerScore++;
+			}else if(shoot(computerGrid, ch[0] - 64 - 1, ch[1] - 48) == 2) { //If invalid guess
+				endTurn = 0;
+			}else{ //If miss
+				endTurn = 1;
+				playerGuessGrid[ch[0] - 64 - 1][ch[1] - 48] = 3;
+				computerGrid[ch[0] - 64 - 1][ch[1] - 48] = 3;
+			}
+            mvprintw(14, 26, "               "); //remove previous input
+		} while (!endTurn);
+		
+		displayCurrentBoard(playerGrid, playerBoardX, playerBoardY);
+		displayCurrentBoard(playerGuessGrid, playerGuessX, playerGuessY);
+		//displayCurrentBoard(computerGrid, cpuBoardX, cpuBoardY); //Comment when actually playing
+		//displayCurrentBoard(computerGuessGrid, cpuGuessX, cpuGuessY); //Comment when actually playing
+
+        //bot guessing
+        while(1){
+            int xGuess = rand() % 10;
+            int yGuess = rand() % 10;
+            //if that coord is a ship mark with hit and mark up the score
+            if (playerGrid[xGuess][yGuess] == 1){
+                playerGrid[xGuess][yGuess] = 2;
+                computerGuessGrid[xGuess][yGuess] = 2;
+                computerScore++;
+                break;    
+            }
+            //if that coord is blank mark ship
+            else if (playerGrid[xGuess][yGuess] == 0){
+                playerGrid[xGuess][yGuess] = 3;
+                computerGuessGrid[xGuess][yGuess] = 3;
+                break;
+            }
         }
+		displayCurrentBoard(playerGrid, playerBoardX, playerBoardY);
+		displayCurrentBoard(playerGuessGrid, playerGuessX, playerGuessY);
+		//displayCurrentBoard(computerGrid, cpuBoardX, cpuBoardY); //Comment when actually playing
+		//displayCurrentBoard(computerGuessGrid, cpuGuessX, cpuGuessY); //Comment when actually playing
+
+		/*
+		if (playerScore == 6) {
+			mvprintw(20, 0, "You Win!");
+		}
+		else if (computerScore == 6) {
+			mvprintw(20, 0, "The Computer Beat You :(");
+		}
+		*/
     }
 
     //Exit screen
     endwin();
+
+	if (playerScore == 6) {
+		printf("You Win!\n");
+	} else {
+		printf("The Computer Beat You :(\n");
+	}
 }
